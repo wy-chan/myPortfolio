3ecfe0d5c812376dda23ef66c46a6a6803fd07b2
wy-chan <79743156+wy-chan@users.noreply.github.com>
2021-11-24 18:56:21 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 
-var currentSlide = 0;
+var currentSlide = [0,0];
 
 function moveRight(i, max, boxWidth) {
-  document.getElementsByClassName("slide")[i].getElementsByClassName("slide-box")[currentSlide].style.opacity = "0.3";
-  currentSlide += 1; 
+  document.getElementsByClassName("slide")[i].getElementsByClassName("slide-box")[currentSlide[i]].style.opacity = "0.3";
+  currentSlide[i] += 1; 
 
   showSlide(i, max, boxWidth);
 
 }
 function moveLeft(i, max, boxWidth) {
-  document.getElementsByClassName("slide")[i].getElementsByClassName("slide-box")[currentSlide].style.opacity = "0.3";
-  currentSlide -= 1; 
+  document.getElementsByClassName("slide")[i].getElementsByClassName("slide-box")[currentSlide[i]].style.opacity = "0.3";
+  currentSlide[i] -= 1; 
 
   showSlide(i, max, boxWidth);
 }
 
 function showSlide(i, max, boxWidth) {
-let shift = currentSlide * -boxWidth;
+let shift = currentSlide[i] * -boxWidth;
  
-switch(currentSlide){
+switch(currentSlide[i]){
     case parseInt(max): 
     document.getElementsByClassName("slide-right")[i].disabled = true;
     document.getElementsByClassName("slide")[i].style.transform =  "translate("+shift+"px)";
@@ -32,5 +32,5 @@ switch(currentSlide){
     document.getElementsByClassName("slide-left")[i].disabled = false;
     document.getElementsByClassName("slide")[i].style.transform =  "translate("+shift+"px)";
 }
-    document.getElementsByClassName("slide")[i].getElementsByClassName("slide-box")[currentSlide].style.opacity = "1";
+    document.getElementsByClassName("slide")[i].getElementsByClassName("slide-box")[currentSlide[i]].style.opacity = "1";
 }
